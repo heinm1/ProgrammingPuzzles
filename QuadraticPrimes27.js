4027ac6a10417749e866ae6783c3d4dce242b12d
k1502670 <k1502670@kcl.ac.uk>
2018-07-15 11:14:24 +0100

    compleat

--- QuadraticPrimes27.js
+++ QuadraticPrimes27.js
@@ -1,3 +1,11 @@
+//Considering quadratics of the form:
+//
+//    n2+an+b, where |a|<1000 and |b|≤1000
+
+//Find the product of the coefficients, a and b, for the quadratic expression that produces the
+//maximum number of primes for consecutive values of n, starting with n=0.
+
+//==========================================================
 
 
 function isPrime(n){
