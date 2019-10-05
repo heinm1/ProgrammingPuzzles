e4449960c65753bee2a7513052a2516f5f1d73bc
Max Hein <maxhein1@hotmail.co.uk>
2019-10-05 19:24:57 +0100

    Solution 1: Compleate

---
+++ src/leet/TwoSum.java
@@ -0,0 +1,25 @@
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] options = new int[]{11, 15, 2, 7};
+        int target = 9;
+
+        int[] result = twoSum(options, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+
+    static int[] twoSum(int[] nums, int target) {
+
+        int length = nums.length;
+        for(int i = 0; i < length; ++i) {
+            for(int j = i+1; j < length; ++j) {
+                int sum = nums[i] + nums[j];
+
+                if(sum == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+}
