945c79a4ce85669e054782fea41ccb083e1fd005
Max Hein <maxhein1@hotmail.co.uk>
2019-10-05 19:26:08 +0100

    Solution 1: Compleate

--- src/leet/TwoSum1.java
+++ src/leet/TwoSum1.java
@@ -1,3 +1,12 @@
+
+
+/*
+  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+
 public class TwoSum1 {
     public static void main(String[] args) {
         int[] options = new int[]{11, 15, 2, 7};
